3d6db45f635fd9df49abacdfbc38ab1070a9b926
Castelianoads <carlosecdpaula@gmail.com>
2020-10-31 16:58:29 -0300

    alteraçao

--- Aula28102020.java
+++ Aula28102020.java
@@ -1,5 +1,5 @@
 
-public class Aula28102020 {
+public class Aula28102020 { 
     public static void main(String[] args) throws Exception {    
         Carro carro1;  //Instância da outra classe
         Carro carro2;  //Instância da outra classe
@@ -8,7 +8,7 @@ public class Aula28102020 {
         carro1 = new Carro("VW", "Gol", 2008, "Branco"); //criando carro
         carro1.status();
 
-        carro2 = new Carro("Fiat", "Palio", 2019, "Preto");  //criando carro
+        carro2 = new Carro("Fiat", "Palio", 2019, "Preto"); //Poderia pedir as informações para o usuario ?
         carro2.status();
     }
 }
